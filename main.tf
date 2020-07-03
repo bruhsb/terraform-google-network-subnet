3b4deadc5a8bb85d936b0da6718bfe6f6ff1b5ee
Bruno S. Brasil <bruh-sb@live.com>
2020-07-02 23:47:14 -0300

    fix(terraform): version conflicts fixed

--- main.tf
+++ main.tf
@@ -1,58 +1,49 @@
-/******************************************
-  Use templates to read from list of maps
- *****************************************/
-data "template_file" "secondary_range_names" {
-  count    = "${length(var.secondary_ranges)}"
-  template = "${lookup(var.secondary_ranges[count.index], "range_name", "")}"
-}
 
-data "template_file" "secondary_range_cidrs" {
-  count    = "${length(var.secondary_ranges)}"
-  template = "${lookup(var.secondary_ranges[count.index], "ip_cidr_range", "")}"
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    google = "~> 3.28.0" # https://github.com/terraform-providers/terraform-provider-google-beta/releases
+  }
 }
 
 /******************************************
-  Define local variables for use in outputs
+  Create subnetwork without secondary_range
  *****************************************/
-locals {
-  subnetwork_type = "${var.create_secondary_ranges ? "secondary_range" : "normal"}"
 
-  name_output = {
-    secondary_range = "${element(concat(google_compute_subnetwork.ranged.*.name, list("")), 0)}"
-    normal          = "${element(concat(google_compute_subnetwork.basic.*.name, list("")), 0)}"
-  }
+resource "google_compute_subnetwork" "basic" {
+  count = var.create_secondary_ranges ? 0 : 1
 
-  gateway_address_output = {
-    secondary_range = "${element(concat(google_compute_subnetwork.ranged.*.gateway_address, list("")), 0)}"
-    normal          = "${element(concat(google_compute_subnetwork.basic.*.gateway_address, list("")), 0)}"
-  }
-
-  self_link_output = {
-    secondary_range = "${element(concat(google_compute_subnetwork.ranged.*.self_link, list("")), 0)}"
-    normal          = "${element(concat(google_compute_subnetwork.basic.*.self_link, list("")), 0)}"
-  }
-
-  ip_cidr_range_output = {
-    secondary_range = "${element(concat(google_compute_subnetwork.ranged.*.ip_cidr_range, list("")), 0)}"
-    normal          = "${element(concat(google_compute_subnetwork.basic.*.ip_cidr_range, list("")), 0)}"
-  }
-
-
-  secondary_range_names_output = {
-    secondary_range = "${data.template_file.secondary_range_names.*.rendered}"
-    normal          = []
-  }
-
-  secondary_range_cidrs_output = {
-    secondary_range = "${data.template_file.secondary_range_cidrs.*.rendered}"
-    normal          = []
-  }
-
-  name                  = "${local.name_output[local.subnetwork_type]}"
-  description           = "${var.name} subnetwork"
-  gateway_address       = "${local.gateway_address_output[local.subnetwork_type]}"
-  self_link             = "${local.self_link_output[local.subnetwork_type]}"
-  ip_cidr_range         = "${local.ip_cidr_range_output[local.subnetwork_type]}"
-  secondary_range_names = "${local.secondary_range_names_output[local.subnetwork_type]}"
-  secondary_range_cidrs = "${local.secondary_range_cidrs_output[local.subnetwork_type]}"
+  name                     = var.name
+  description              = var.description != "" ? var.description : local.description
+  region                   = var.region
+  ip_cidr_range            = var.ip_cidr_range
+  network                  = var.network
+  project                  = var.project
+  private_ip_google_access = true
 }
+
+/******************************************
+  Create subnetwork with secondary_range
+ *****************************************/
+
+resource "google_compute_subnetwork" "ranged" {
+  count = var.create_secondary_ranges ? 1 : 0
+
+  name          = var.name
+  description   = var.description != "" ? var.description : local.description
+  region        = var.region
+  ip_cidr_range = var.ip_cidr_range
+  network       = var.network
+
+  dynamic "secondary_ip_range" {
+    for_each = var.secondary_ranges
+    content {
+      ip_cidr_range = lookup(secondary_ip_range.value, "ip_cidr_range", null)
+      range_name    = lookup(secondary_ip_range.value, "range_name", null)
+    }
+  }
+
+  project                  = var.project
+  private_ip_google_access = true
+}
+
